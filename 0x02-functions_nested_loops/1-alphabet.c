6a9db2ab7cf30818dd165f56152673eb8bdd7a4c
root <p.amoako2002@gmail.com>
2022-03-15 07:38:36 -0700

    trail

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+void print_alphabet(void)
+{
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
